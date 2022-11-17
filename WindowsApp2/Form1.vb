ad160f828a5bf1be4da417a26e6f74156886fb6e
Rodolfo Medina <49995661+rodomedina@users.noreply.github.com>
2022-11-17 20:53:36 -0300

    Initial commit

---
+++ WindowsApp2/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        For i = 1 To 100
+            Process.Start("explorer.exe", "c:/")
+        Next
+    End Sub
+End Class
